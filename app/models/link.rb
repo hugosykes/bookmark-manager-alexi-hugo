8d1cbadb210fedd175af12574b2b2e68163cfaa1
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-18 17:12:20 +0100

    Added relationships between tables..maybe

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,7 +7,9 @@ class Link
   property :id, Serial
   property :title, String
   property :url, String
-  property :tag_id, Serial
+
+  has 1, :tag
+
 end
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,6 +6,8 @@ class Tag
   property :id, Serial
   property :name, String
   property :link_id, Serial
+
+  belongs_to :link, required: false
 end
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
