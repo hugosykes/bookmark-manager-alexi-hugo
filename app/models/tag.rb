62dac26e35167f5e33ac18b8b9ff61a4bf0aa1c9
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-18 16:57:36 +0100

    New stuff

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,14 +1,13 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
 
-class Link
+
+class Tag
   include DataMapper::Resource
 
   property :id, Serial
-  property :title, String
-  property :url, String
-  property :tag_id, Serial
+  property :name, String
+  property :link_id, Serial
 end
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
