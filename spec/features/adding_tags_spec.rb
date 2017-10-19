df11e1895b5a57240d6fddec66e2449da4a78b59
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-19 16:01:44 +0100

    Added ability to use multiple tags and tested

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -5,10 +5,20 @@ feature 'Tags' do
     visit '/links/new'
     fill_in 'url', with: 'www.googlie.com'
     fill_in 'title', with: 'Googlieasdf'
-    fill_in 'tags', with: 'search engine'
+    fill_in 'tags', with: 'search'
     click_button 'Create link'
     link = Link.first
 
-    expect(link.tags.map(&:name)).to include('search engine')
+    expect(link.tags.map(&:name)).to include('search')
+  end
+
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags', with: 'education ruby'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
   end
 end
